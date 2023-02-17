17aee6c646aa5ef26dd334c48216332ca065be04
mmdybdahl <97943841+mmdybdahl@users.noreply.github.com>
2023-02-17 09:58:25 +0100

    Update App.java
    ÆNdring i 6 på Git Hub

--- Mod_01/src/model/App.java
+++ Mod_01/src/model/App.java
@@ -3,7 +3,7 @@ package model;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hej");
-        System.out.println("Igen");
+        System.out.println("Igen dfjhasdkjfkasfkjsadfkj");
         //Kommentar
     }
 }
